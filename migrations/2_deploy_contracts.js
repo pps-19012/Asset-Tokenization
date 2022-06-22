521d79e1b1f056f94546e5cffdbfe539b8b223c2
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2022-06-22 12:42:29 +0530

    adding hook and modifying unit tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,14 @@
 var Pokens = artifacts.require("./Pokens.sol");
 var TokenSale = artifacts.require("./TokenSale.sol");
+require("dotenv").config({path: "../.env"});
+//console.log(process.env);
 
 module.exports = async function(deployer) {
     let addr = await web3.eth.getAccounts();
+    //await deployer.deploy(Pokens, process.env.INITIAL_TOKENS);
     await deployer.deploy(Pokens, 1000000);
     await deployer.deploy(TokenSale, 1, addr[0], Pokens.address);
     let instance = await Pokens.deployed();
+    //await instance.transfer(TokenSale.address, process.env.INITIAL_TOKENS);
     await instance.transfer(TokenSale.address, 1000000);
 }
--- test/Poken.test.js
+++ test/Poken.test.js
@@ -10,32 +10,40 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
+require("dotenv").config({path: "../.env"});
+
 contract("TokenTest", async (accounts) => {
 
     const [deployerAccount, recipientAccount, anotherAccount] = accounts;
 
+    beforeEach(async () => {
+        //this.poken = await Pokens.new(process.env.INITIAL_TOKENS);
+        this.poken = await Pokens.new(1000000);
+    })
+    
     it("All tokens should be in deployer account", async () => {
-        let instance = await Pokens.deployed();
+        let instance = this.poken;
         let totalSupply = await instance.totalSupply();
         //let balance = await instance.balanceOf(accounts[0]);
         //assert.equal(balance.valuOf(), initialSupply.valueOf(), "The balance was not the same");
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     })
 
     it("It is possible to send tokens between accounts", async () => {
         const sendTokens = 1;
-        let instance = await Pokens.deployed();
+        let instance = this.poken;
         let totalSupply = await instance.totalSupply();
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
-        expect(instance.transfer(recipientAccount, sendTokens)).to.eventually.be.fulfilled;
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
-        expect(instance.balanceOf(recipientAccount)).to.eventually.be.bignumber.equal(new BN(sendTokens));
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
+        await expect(instance.transfer(recipientAccount, sendTokens)).to.eventually.be.fulfilled;
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+        await expect(instance.balanceOf(recipientAccount)).to.eventually.be.bignumber.equal(new BN(sendTokens));
     })
 
     it("It is not possible to send more tokens than total", async () => {
-        let instance = await Pokens.deployed();
+        let instance = this.poken;
         let balanceOfDeployer = await instance.balanceOf(deployerAccount);
-        expect(instance.transfer(recipientAccount, new BN(balanceOfDeployer+1))).to.eventually.be.rejectedWith("noice");
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
+        await expect(instance.transfer(recipientAccount, new BN(balanceOfDeployer+1))).to.eventually.be.rejected;
+        await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
     })
+
 });
