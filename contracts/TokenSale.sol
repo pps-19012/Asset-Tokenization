7d5c2d872c28ce9712b47f9347e3a736f8ebcd2b
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2022-06-22 11:47:42 +0530

    first commit

---
+++ contracts/TokenSale.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import "./Crowdsale.sol";
+
+contract TokenSale is Crowdsale {
+    constructor(
+        uint256 rate, // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    ) public Crowdsale(rate, wallet, token) {}
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
-var MyToken = artifacts.require("./Pokens.sol");
+var Pokens = artifacts.require("./Pokens.sol");
+var TokenSale = artifacts.require("./TokenSale.sol");
 
 module.exports = async function(deployer) {
-    await deployer.deploy(MyToken, 1000000);
+    let addr = await web3.eth.getAccounts();
+    await deployer.deploy(Pokens, 1000000);
+    await deployer.deploy(TokenSale, 1, addr[0], Pokens.address);
+    let instance = await Pokens.deployed();
+    await instance.transfer(TokenSale.address, 1000000);
 }
