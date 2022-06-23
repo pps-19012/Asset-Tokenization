c2bb71141a1e6373ed88e959574db7f8e1659202
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2022-06-24 00:05:41 +0530

    adding 2 unit tests to tokensale

--- test/TokenSale.test.js
+++ test/TokenSale.test.js
@@ -14,4 +14,21 @@ contract("TokenSaleTest", async (accounts) => {
         let instance = await Pokens.deployed();
         await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
     })
+
+    it("All tokens should be in the token sale  smart contract by default", async () => {
+        let instance = await Pokens.deployed();
+        let balanceOfTokenSaleSmartContract = await instance.balanceOf(TokenSale.address);
+        let totalSupply = await instance.totalSupply();
+        await expect(balanceOfTokenSaleSmartContract).to.be.a.bignumber.equal(totalSupply);
+    })
+
+    it("It should be possible to trade tokens", async () => {
+        const sendTokens = 1;
+        let tokenInstance = await Pokens.deployed;
+        let tokensaleInstance = await TokenSale.deployed();
+        let balanceBefore = await tokenInstance.balanceOf.call(recipientAccount);
+        await expect(tokensaleInstance.sendTransaction({form: deployerAccount, value: web3.utils.toWei(sendTokens, "wei")})).to.be.fulfilled;
+        //await expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceBefore.add(new BN(sendTokens)));
+        await expect(balanceBefore + new BN(sendTokens)).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipientAccount));
+    })
 });
