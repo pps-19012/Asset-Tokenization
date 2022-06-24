bfb074e9be18a9a7f25dad3a3f83005e7633b6b1
Pushpendra Pratap Singh <pushpendrasingh.19012@gmail.com>
2022-06-24 17:30:05 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -53,16 +53,9 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
+        <h1>Poken Sale!</h1>
+        <p>Get your pokens today!</p>
+        <h2>KYC Whitelisting</h2>
         <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
